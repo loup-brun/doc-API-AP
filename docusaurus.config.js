d53fced27e55c282347ca90ae9edc8039be97846
rdelannay <roch.delannay@umontreal.ca>
2022-10-24 08:36:56 -0400

    try to fix deployment on gh-pages 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'RochDLY', // Usually your GitHub org/user name.
   projectName: 'doc-API-AP', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
